82ce6b16134d41aa10958477a6ddf98d3b707657
Robin Carter <Robin.Carter@sse.com>
2020-07-02 17:04:50 +0100

    Update api to return facebook places

---
+++ FacebookApi.Client/PlacesResponse.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace FacebookApi.Client
+{
+    public partial class PlacesResponse
+    {
+        [JsonProperty("data")]
+        public List<Place> Venues { get; set; }
+    }
+
+    public partial class Place
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("location")]
+        public Location Location { get; set; }
+
+        [JsonProperty("about")]
+        public string About { get; set; }
+
+        [JsonProperty("category_list")]
+        public List<CategoryList> CategoryList { get; set; }
+
+        [JsonProperty("description", NullValueHandling = NullValueHandling.Ignore)]
+        public string Description { get; set; }
+
+        [JsonProperty("hours", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Hour> Hours { get; set; }
+
+        [JsonProperty("overall_star_rating", NullValueHandling = NullValueHandling.Ignore)]
+        public double? OverallStarRating { get; set; }
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("single_line_address")]
+        public string SingleLineAddress { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+
+    public partial class CategoryList
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public partial class Hour
+    {
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+
+    public partial class Location
+    {
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("street")]
+        public string Street { get; set; }
+
+        [JsonProperty("zip")]
+        public string Zip { get; set; }
+    }
+
+    public partial class PlacesResponse
+    {
+        public static PlacesResponse FromJson(string json) => JsonConvert.DeserializeObject<PlacesResponse>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this PlacesResponse self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
--- FacebookApi/FacebookController.cs
+++ FacebookApi/FacebookController.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Net.Http;
+using FacebookApi.Client;
 
 namespace FacebookEventsApi
 {
@@ -33,8 +34,8 @@ namespace FacebookEventsApi
 
             try
             {
-                //var response = await _client.GetAsync($"https://api.foursquare.com/v2/venues/search?client_id={_clientId}&client_secret={_clientSecret}&v={_v}&near=new york&intent=browse&radius=10000&limit=10");
-                //response.EnsureSuccessStatusCode();
+                var response = await _client.GetAsync($"https://graph.facebook.com/search?type=place&center=51.1375982,-3.0087667&distance=1&access_token=566945730659600|J0GkU5iDaQGU8pMhJ9gSmW5pfrg");
+                response.EnsureSuccessStatusCode();
             }
             catch (Exception exception)
             {
@@ -42,6 +43,45 @@ namespace FacebookEventsApi
             }
 
             return new OkObjectResult(HealthCheckResult.Healthy("A healthy result."));
+        }
+
+        [FunctionName("Places")]
+        public async Task<IActionResult> RunGetPlaces(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+           ILogger log)
+        {
+            PlacesResponse result = new PlacesResponse();
+
+            try
+            {
+                log.LogInformation("C# HTTP trigger function processed a request.");
+
+                string name = req.Query["name"];
+
+                var latQueryParameter = req.Query["lat"].ToString().Trim('f', 'F');
+                if (string.IsNullOrEmpty(latQueryParameter))
+                    return new BadRequestObjectResult("Please pass a lat parameter");
+
+                var lonQueryParameter = req.Query["lon"].ToString().Trim('f', 'F');
+                if (string.IsNullOrEmpty(lonQueryParameter))
+                    return new BadRequestObjectResult("Please pass a lon parameter");
+
+                string radius = "10000";
+                var radiusQueryParameter = req.Query["radius"].ToString();
+                if (!string.IsNullOrEmpty(radiusQueryParameter))
+                    radius = radiusQueryParameter;
+
+                _client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
+                var response = await _client.GetAsync($"https://graph.facebook.com/search?type=place&center={latQueryParameter},{lonQueryParameter}&distance={radius}&fields=name,location,about,category_list,description,hours,overall_star_rating,phone,restaurant_services,restaurant_specialties,single_line_address&access_token=566945730659600|J0GkU5iDaQGU8pMhJ9gSmW5pfrg");
+                response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                result = await response.Content.ReadAsAsync<PlacesResponse>();
+            }
+            catch (Exception exception)
+            {
+                // Error
+            }
+
+            return new OkObjectResult(result);
         }
     }
 }
